c214e6900cfc6b959e62caec477ebe446d1d7daf
Mischka Booley <mischka@gearbox.com.au>
2022-12-17 15:22:47 +1100

    refactor actions by adding exception handling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,49 +1,48 @@
 class MoviesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  # GET /movies
   def index
     render json: Movie.all, status: :ok
   end
 
+  # GET /movies/:id
   def show
-    movie = Movie.find_by(id: params[:id])
-    if movie
-      render json: movie, status: :ok
-    else
-      render json: { error: 'Movie not found' }, status: :not_found
-    end
+    movie = find_movie
+    render json: movie, status: :ok
   end
 
+  # POST /movies
   def create
     movie = Movie.create(movie_params)
     render json: movie, status: :created
   end
 
+  # PATCH /movies/:id
   def update
-    #find
-    movie = Movie.find_by(id: params[:id])
-    if movie
-      #update
-      movie.update(movie_params)
-      render json: movie, status: :accepted
-    else
-      render json: { error: 'Movie not found' }, status: :not_found
-    end
+    movie = find_movie
+    movie.update(movie_params)
+    render json: movie, status: :accepted
   end
 
+  # DELETE /movies/:id
   def destroy
-    #find
-    movie = Movie.find_by(id: params[:id])
-    if movie
-      #delete
-      movie.destroy
-      head :no_content
-    else
-      render json: { error: 'Movie not found' }, status: :not_found
-    end
+    movie = find_movie
+    movie.destroy
+    head :no_content
   end
 
   private
 
   def movie_params
     params.permit(:title, :overview, :director, :image)
+  end
+
+  def find_movie
+    Movie.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: 'Movie not found' }, status: :not_found
   end
 end
