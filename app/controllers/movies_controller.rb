cb735fe1941c89790d3c7003b30367d85daf8dd4
Mischka Booley <mischka@gearbox.com.au>
2022-12-17 13:42:06 +1100

    add Movie index and show actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,14 @@
 class MoviesController < ApplicationController
+  def index
+    render json: Movie.all, status: :ok
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+        render json: movie, status: :ok
+    else
+      render json: { error: 'Movie not found'}, status: :not_found
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :reviews
-  resources :movies
+  # resources :reviews
+  resources :movies, only: [:index, :show]
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
