3d76e0656458c4b0323cd382227366413689eef1
Mischka Booley <mischka@gearbox.com.au>
2022-12-17 20:59:01 +1100

    add user resource, reference and relations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  # resources :users
   # resources :reviews
   resources :movies, only: [:index, :show, :create, :update, :destroy]
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_17_014022) do
+ActiveRecord::Schema.define(version: 2022_12_17_094447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,19 @@ ActiveRecord::Schema.define(version: 2022_12_17_014022) do
     t.bigint "movie_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "reviews", "movies"
+  add_foreign_key "reviews", "users"
 end
