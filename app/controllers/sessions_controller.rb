a66d73c764e09bdc28a0c3487d59239270ab8430
Mischka Booley <mischka@gearbox.com.au>
2022-12-18 12:47:11 +1100

    remove rescue statement from reviews controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # login
   def create
-    user = User.find_by!(username: params[:username])
-    if user.authenticate(params[:password])
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: user, status: :created
     else
-      render json: { error: "Password invalid" }, status: :unauthorized
+      render json: { error: {login: 'Invalid username or password'} }, status: :unauthorized
     end
   end
 
@@ -15,11 +14,5 @@ class SessionsController < ApplicationController
   def destroy
     session.delete :user_id
     head :no_content
-  end
-
-  private
-
-  def render_not_found_response
-    render json: { error: 'User not found' }, status: :not_found
   end
 end
